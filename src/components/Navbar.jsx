714f1ba3920503be370ce2a5d7ebdc834c0f6326
danielAwale <danielmadhukarawale@gmail.com>
2022-12-01 19:52:43 -0800

    tailwind css added to sign up button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
       <div>
         <button>Sign In</button>
-        <button>Sign Up</button>
+        <button className='bg-red-600 px-6 py-2 text-white rounded cursor-pointer'>Sign Up</button>
       </div>
     </div>
   )
