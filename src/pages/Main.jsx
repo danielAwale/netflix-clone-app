9709f5fa3fbc73c6470e6daa078ea99157b88f72
danielAwale <danielmadhukarawale@gmail.com>
2022-12-02 14:11:16 -0800

    contain a function that will set a random movie from the bunch

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,12 +5,15 @@ import requests from '../requests';
 const Main = () => {
   const [movies, setMovies] = useState([]);
 
+  const movie = movies[Math.floor(Math.random() * movies.length)]
+
   useEffect(() => {
     axios.get(requests.requestPopular)
-    .then((response) => {setMovies(response.data)})
+    .then((response) => {setMovies(response.data.results)})
   }, [])
 
-  
+  console.log(movie)
+
   return (
     <div>Main</div>
   )
